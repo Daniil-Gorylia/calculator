36e79d5058f94e7bc5d625ca3de2b146396075c0
Daniil <zzzgahihzzz@gmail.com>
2022-03-03 14:46:12 +0300

    save me

---
+++ main.py
@@ -0,0 +1,29 @@
+import operations
+
+number_1 = float(input('Введите любое первое число'))
+my_operators = input('Введите оператор (+, -, /, *, mod, pow, div)')
+number_2 = float(input('Введите любое второе число'))
+counter = 0
+
+if my_operators == '+':
+    counter = number_1 + number_2
+elif my_operators == '-':
+    counter = number_1 - number_2
+elif my_operators == '/':
+    counter = number_1 / number_2
+elif my_operators == '*':
+    counter = number_1 * number_2
+elif my_operators == 'mod':
+    counter = number_1 % number_2
+elif my_operators == 'pow':
+    counter = pow(number_1, number_2)
+elif my_operators == 'div':
+    counter = number_1 // number_2
+
+
+if counter.is_integer() == True:
+    print(int(counter))
+else:
+    print(counter)
+
+
