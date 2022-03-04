37dfa2a52ce72a8f1991801b15da32c6013b0cdc
Daniil <zzzgahihzzz@gmail.com>
2022-03-04 21:01:21 +0300

    pep

--- operations.py
+++ operations.py
@@ -1,5 +1,5 @@
 def number_to_words(n):
-    number1 = ('ноль','один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять')
+    number1 = ('ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять')
     number2 = (None, 'десять', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят',
                'девяносто')
     number3 = (None, 'одинадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать',
@@ -15,7 +15,7 @@ def number_to_words(n):
         return number1[n]
     elif 10 < n < 20:
         return number3[n - 10]
-    elif n >= 10 and n < 100 and d.is_integer() and number2[int(d)] in number2:
+    elif 10 <= n < 100 and d.is_integer() and number2[int(d)] in number2:
         return number2[int(d)]
     elif 20 < n < 100:
         return number2[int(n2 / 10)] + ' ' + number1[n1]
@@ -25,23 +25,23 @@ def number_to_words(n):
         return number4[int(d1)] + ' ' + number1[n1]
     elif 110 < n < 120:
         return number4[int(d1)] + ' ' + number3[n - 110]
-    elif n >= 110 and n < 200 and n1 == 0 and number4[int(d1)] in number4:
-        return  number4[int(d1)] + ' ' + number2[int(n3 / 10)]
+    elif 110 <= n < 200 and n1 == 0 and number4[int(d1)] in number4:
+        return number4[int(d1)] + ' ' + number2[int(n3 / 10)]
     elif 120 < n < 200:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)] + ' ' + number1[n1]
     elif 200 < n < 210:
         return number4[int(d1)] + ' ' + number1[n1]
     elif 210 < n < 220:
         return number4[int(d1)] + ' ' + number3[n - 210]
-    elif n >= 210 and n < 300 and n1 == 0 and number4[int(d1)] in number4:
-        return  number4[int(d1)] + ' ' + number2[int(n3 / 10)]
+    elif 210 <= n < 300 and n1 == 0 and number4[int(d1)] in number4:
+        return number4[int(d1)] + ' ' + number2[int(n3 / 10)]
     elif 220 < n < 300:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)] + ' ' + number1[n1]
     elif 300 < n < 310:
         return number4[int(d1)] + ' ' + number1[n1]
     elif 310 < n < 320:
         return number4[int(d1)] + ' ' + number3[n - 310]
-    elif n >= 310 and n < 400 and n1 == 0 and number4[int(d1)] in number4:
+    elif 310 <= n < 400 and n1 == 0 and number4[int(d1)] in number4:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)]
     elif 320 < n < 400:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)] + ' ' + number1[n1]
@@ -49,7 +49,7 @@ def number_to_words(n):
         return number4[int(d1)] + ' ' + number1[n1]
     elif 410 < n < 420:
         return number4[int(d1)] + ' ' + number3[n - 410]
-    elif n >= 410 and n < 500 and n1 == 0 and number4[int(d1)] in number4:
+    elif 410 <= n < 500 and n1 == 0 and number4[int(d1)] in number4:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)]
     elif 420 < n < 500:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)] + ' ' + number1[n1]
@@ -57,7 +57,7 @@ def number_to_words(n):
         return number4[int(d1)] + ' ' + number1[n1]
     elif 510 < n < 520:
         return number4[int(d1)] + ' ' + number3[n - 510]
-    elif n >= 510 and n < 600 and n1 == 0 and number4[int(d1)] in number4:
+    elif 510 <= n < 600 and n1 == 0 and number4[int(d1)] in number4:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)]
     elif 520 < n < 600:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)] + ' ' + number1[n1]
@@ -65,7 +65,7 @@ def number_to_words(n):
         return number4[int(d1)] + ' ' + number1[n1]
     elif 610 < n < 620:
         return number4[int(d1)] + ' ' + number3[n - 710]
-    elif n >= 610 and n < 700 and n1 == 0 and number4[int(d1)] in number4:
+    elif 610 <= n < 700 and n1 == 0 and number4[int(d1)] in number4:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)]
     elif 620 < n < 700:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)] + ' ' + number1[n1]
@@ -73,7 +73,7 @@ def number_to_words(n):
         return number4[int(d1)] + ' ' + number1[n1]
     elif 710 < n < 720:
         return number4[int(d1)] + ' ' + number3[n - 710]
-    elif n >= 710 and n < 800 and n1 == 0 and number4[int(d1)] in number4:
+    elif 710 <= n < 800 and n1 == 0 and number4[int(d1)] in number4:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)]
     elif 720 < n < 800:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)] + ' ' + number1[n1]
@@ -81,7 +81,7 @@ def number_to_words(n):
         return number4[int(d1)] + ' ' + number1[n1]
     elif 810 < n < 820:
         return number4[int(d1)] + ' ' + number3[n - 810]
-    elif n >= 810 and n < 900 and n1 == 0 and number4[int(d1)] in number4:
+    elif 810 <= n < 900 and n1 == 0 and number4[int(d1)] in number4:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)]
     elif 820 < n < 900:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)] + ' ' + number1[n1]
@@ -89,9 +89,7 @@ def number_to_words(n):
         return number4[int(d1)] + ' ' + number1[n1]
     elif 910 < n < 920:
         return number4[int(d1)] + ' ' + number3[n - 910]
-    elif n >= 910 and n < 1000 and n1 == 0 and number4[int(d1)] in number4:
-        return  number4[int(d1)] + ' ' + number2[int(n3 / 10)]
+    elif 910 <= n < 1000 and n1 == 0 and number4[int(d1)] in number4:
+        return number4[int(d1)] + ' ' + number2[int(n3 / 10)]
     elif 920 < n < 1000:
         return number4[int(d1)] + ' ' + number2[int(n3 / 10)] + ' ' + number1[n1]
-
-
